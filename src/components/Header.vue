df645d8e59deb1be5e684b387ea9f0da767bd578
Kirill Ilinsky <kirill.ilinsky@gmail.com>
2021-03-31 10:53:47 +0300

    asd

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,19 +3,47 @@
     <router-link to="/" class="header__logo" tag="span">Star Worst</router-link>
     <nav class="header-nav">
       <router-link class="header-nav__link" to="/">Home</router-link>
-      <router-link class="header-nav__link" to="/signup">Sign Up</router-link>
-      <router-link class="header-nav__link" to="/profile">Profile</router-link>
+
+      <router-link v-if="login" class="header-nav__link" to="/profile"
+        >Profile</router-link
+      >
+      <router-link v-else class="header-nav__link" to="/signup"
+        >Sign Up</router-link
+      >
+      <button v-if="login" @click="redirect" class="header-nav__link">
+        Log out
+      </button>
     </nav>
   </header>
 </template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  methods: {
+    ...mapActions({ loginAction: "loginAction" }),
+    redirect() {
+      this.loginAction();
+      this.$router.push("/signup");
+    },
+  },
+  computed: {
+    ...mapGetters({ login: "getUserLogged", active: "getActive" }),
+  },
+};
+</script>
 
 <style lang="scss" scoped>
-  .header {
-    flex: 0 0 auto;
-    &-nav__link {
-      &.router-link-exact-active {
-        color: rgb(26, 92, 255);
-      }
+.header {
+  flex: 0 0 auto;
+  &-nav__link {
+    background: transparent;
+    color: #fff;
+    font: inherit;
+    border: none;
+    &.router-link-exact-active {
+      color: rgb(26, 92, 255);
     }
   }
+}
 </style>
